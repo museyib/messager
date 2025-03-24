16a45684d219f708a61332ec351acb0aaa418b32
museyib <museyib.e@gmail.com>
2025-03-25 01:06:55 +0400

    Netlify

--- web-client/vite.config.ts
+++ web-client/vite.config.ts
@@ -3,9 +3,10 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import {reactRouter} from "@react-router/dev/vite";
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
   define: {
     global: "window",
   },
